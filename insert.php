e167eae210e15fee53bdfd5809b038d4b3b3aa5d
karthivels <karthi.vels@gmail.com>
2020-12-30 23:17:30 +0530

    Update insert.php
    insert query is incorrect

--- insert.php
+++ insert.php
@@ -1,57 +1,57 @@
 <?php
 
-$con=new mysqli('localhost','root','','mobile');
+$con = new mysqli('localhost', 'root', '', 'mobile');
 
-if ($con->connect_errno)
-{
-	echo $con->connect_error;
-	die();
+if ($con->connect_errno) {
+    echo $con->connect_error;
+    die();
 }
 
 ?>
 <html>
 <head>
-<title>mobile</title>
+    <title>mobile</title>
 </head>
 <body>
 <form action="insert.php" method="post">
-<table>
-	<tr><td>Model</td><td><input type="text" name="Model"></td></tr>
-	<tr><td>RAM</td><td><input type="text" name="RAM"></td></tr>
-	<tr><td>ROM</td><td><input type="text" name="ROM"></td></tr>
-	<tr>
-	<td colspan='2' align="center">
-	<input type="submit" name"submit" value="Save Data">
-	</td>
-	</tr>
-</table>
+    <table>
+        <tr>
+            <td>Model</td>
+            <td><input type="text" name="Model"></td>
+        </tr>
+        <tr>
+            <td>RAM</td>
+            <td><input type="text" name="RAM"></td>
+        </tr>
+        <tr>
+            <td>ROM</td>
+            <td><input type="text" name="ROM"></td>
+        </tr>
+        <tr>
+            <td colspan='2' align="center">
+                <input type="submit" name="submit" value="Save Data">
+            </td>
+        </tr>
+    </table>
 </form>
 <?php
-if(isset($_POST['submit']))
-{
-$Model=$_POST['Model'];
-$RAM=$_POST['RAM'];
-$ROM=$_POST['ROM'];
-if($Model!=""&&$RAM!=""&&$ROM!="")
-{
-$sql="INSERT INTO REDMI(Model,RAM,ROM) values ('$Model',$RAM','$ROM')";
-if($con->query($sql))
-{
-	echo "Data stored";
-}
-else
-{
-	echo "Insert data failed";
-}
-}
-else
-{
-	echo"All Feild Required";
-}
-}
-else
-{
-		echo"Please Enter All The Data";
+if (isset($_POST['submit'])) {
+    $Model = $_POST['Model'];
+    $RAM = $_POST['RAM'];
+    $ROM = $_POST['ROM'];
+    if ($Model != "" && $RAM != "" && $ROM != "") {
+        $sql = "INSERT INTO `redmi` (`Model`, `RAM`, `ROM`) VALUES ('$Model','$RAM','$ROM');";
+        echo $sql;
+        if ($con->query($sql)) {
+            echo "Data stored";
+        } else {
+            echo "Insert data failed";
+        }
+    } else {
+        echo "All Feild Required";
+    }
+} else {
+    echo "Please Enter All The Data";
 }
 ?>
 </body>
